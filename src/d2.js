2749a4a5ed941228786b0270f8399e7d25c64647
敏岳 <wuyue.lwy@alibaba-inc.com>
2017-01-19 19:46:21 +0800

    线条处理

--- src/d2.js
+++ src/d2.js
@@ -1,5 +1,6 @@
 //各种2d函数
 import * as d3 from 'd3';
+const lineColor = 'red'
 const d2 = {
 	/**
 	  * @description 直接画一条线
@@ -12,21 +13,21 @@ const d2 = {
 		let line = d3.line()
 			.x((d) => {return d.x;})
 			.y((d) => {return d.y;});
-		let path = d3.select(option.el).append('svg').append('g')
+		let path = option.g
 			.append('path')
 			.attr('stroke-width', '1')
 			.attr('fill', 'transparent')
-			.attr('stroke', 'yellow');
+			.attr('stroke', lineColor);
 		if (option.animate) {
-			let linePoints = [];	
+			let linePoints = [];
 			let points = option.points;
 			for (let i = 0; i < option.points.length - 1; i++) {
-				linePoints = linePoints.concat(this.computePoints({start: points[i], end: points[i + 1]}));	
+				linePoints = linePoints.concat(this.computePoints({start: points[i], end: points[i + 1]}));
 			}
 			this.animateLine({
 				path: path,
-				points: linePoints 
-			});		
+				points: linePoints
+			});
 		} else {
 			path.attr('d', line(option.points))
 		}
@@ -47,23 +48,39 @@ const d2 = {
 		render();
 		function render () {
 			if (i >= length && length < 2) {
-				return;	
+				return;
 			}
-			requestAnimationFrame(render);	
-			option.path.attr('d', line(points.slice(0, i))); 
+			requestAnimationFrame(render);
+			option.path.attr('d', line(points.slice(0, i)));
 			i ++;
-		}	
+		}
+	},
+	//计算贝塞尔曲线的点 简单的二次曲线
+	//option start, end, control
+	computePointsBelizer: function (option) {
+		//公式 p(t) = (1-t) * (1-t) * p0 + 2t(1-t)p1 + t*t*p2
+		let points = [];
+		let step = 0.02;
+		let t = 0;
+		while (t <= 1) {
+			points.push({
+				x:  (1 - t) * (1 - t) * option.start.x + 2 * t * (1 - t) * option.control.x + t * t * option.end.x,
+				y:(1 - t) * (1 - t) * option.start.y + 2 * t * (1 - t) * option.control.y + t * t * option.end.y
+			});
+			t += step;
+		}
+		return points;
 	},
 	/*
 	 * @description 根据起始点 计算中间点 画直线
 	 * @option
 	 *    start,
-	 *    end 
+	 *    end
 	 *    step
 	 */
 	computePoints: function (option) {
 		let params = {
-			step: 0.05,
+			step: 0.02,
 			... option
 		};
 		let points = [];
@@ -71,7 +88,7 @@ const d2 = {
 		let end = params.end;
 		let t = 0;
 		//步长
-		let step = 0.05;
+		let step = params.step;
 		if (start.x == end.x) {
 			//竖直的特殊情况处理
 			let y = d3.interpolate(start.y, end.y);
@@ -79,20 +96,72 @@ const d2 = {
 				points.push({x: start.x, y: y(t)});
 				t += step;
 			}
-			return points;	
-		}	
+			return points;
+		}
 		//斜率
 		let k = (end.y - start.y) / (end.x - start.x);
 		// y = k (x - x0) + y0
 		//差值 (1-t) * x0 + x1 * t 一次线性插值
 		let x = d3.interpolate(start.x, end.x);
 		while (t <= 1) {
-			let tmpX = x(t);	
+			let tmpX = x(t);
 			let tmpY = k * (tmpX - start.x) + start.y;
 			points.push({x: tmpX, y: tmpY});
 			t += step;
 		}
 		return points;
+	},
+	/*
+	 * @description 贝塞尔二次曲线
+	 *  g: 容器
+	 *  start: 端点
+	 *  end: 端点
+	 */
+	belzier: function (option) {
+		let belizier = option.g
+			.append('path')
+			.attr('stroke-width', '1')
+			.attr('fill', 'transparent')
+			.attr('stroke', lineColor);
+		//控制点
+		let control;
+		let start = option.start;
+		let end = option.end;
+		let direction = Math.random() * 2 > 1 ? 1 : -1;
+		//设置控制点
+		if (start.x === end.x) {
+			control = {
+				x: start.x +  direction * Math.abs(end.y - start.y) / 2,
+				y: (start.y + end.y) / 2
+			};
+		} else if (start.y === end.y) {
+			control = {
+				x: (start.x + end.x) / 2,
+				y: start.y + direction * Math.abs(start.x - end.x) / 2
+			};
+		} else {
+			control = {
+				x: start.x,
+				y: (start.y + end.y)  / 2
+			};
+		}
+		if (option.animate) {
+			let points = this.computePointsBelizer({
+				start: start,
+				control: control,
+				end: end
+			});
+			this.animateLine({
+				path: belizier,
+				points: points
+			});
+		} else {
+			let path = d3.path();
+			path.moveTo(start.x, start.y);
+			path.quadraticCurveTo(control.x,control.y, end.x, end.y);
+			belizier.attr('d', path.toString());
+		}
+
 	}
 };
 export default d2;
--- src/line.js
+++ src/line.js
@@ -3,16 +3,46 @@ import $ from 'jquery';
 import d2 from './d2';
 import './line.less';
 //画一条直线
-const w = 400;
-const h = 400;
-let points = [];
-//生成 100个模拟点
-for (let i = 0; i < 100; i++) {
-	points.push({x: Math.random() * w, y: Math.random() * h});	
+const w = $('body').width();
+const h = $('body').height();
+let g = d3.select('#line').append('svg').append('g');
+let type = 1;
+switch (type) {
+	case 0:
+		let points = [];
+		//生成 100个模拟点
+		for (let i = 0; i < 100; i++) {
+			points.push({x: Math.random() * w, y: Math.random() * h});
+		}
+		//动态的画线
+		d2.line({
+			g: g,
+			points: points,
+			animate: true
+		});
+		break;
+	case 1:
+		points = [];
+		let start = {x: Math.random() * w , y: Math.random() * h };
+		let end = {x: Math.random() * w , y: Math.random() * h };
+		d2.belzier({
+			g: g,
+			start: start,
+			end: end,
+			animate: true
+		});
+		//生成 10 条贝塞尔曲线
+		/*
+		for (let j = 0; j < 10; j ++) {
+			let start = {x: Math.random() * w , y: Math.random() * h };
+			let end = {x: Math.random() * w , y: Math.random() * h };
+			d2.belzier({
+				g: g,
+				start: start,
+				end: end,
+				animate: true
+			});
+		}
+		*/
+		break;
 }
-//动态的画一条直线
-d2.line({
-	el: '#line',
-	points: points,
-	animate: true
-});
