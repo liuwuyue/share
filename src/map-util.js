e6a74e23a4a0c4503ac798c7e2d8bbb44f90ca4a
敏岳 <wuyue.lwy@alibaba-inc.com>
2017-02-20 11:05:37 +0800

    地图逻辑处理

--- src/map-util.js
+++ src/map-util.js
@@ -1,7 +1,9 @@
 import * as d3 from 'd3';
+//数据拿的g2的地图数据
 import chinaGeoJson from 'china-geojson';
 let width = window.innerWidth;
 let height = window.innerHeight;
+const NANHAI = '南海诸岛';
 const mapUtil = {
 	china: function () {
 		let projection = d3.geoMercator()
@@ -18,18 +20,23 @@ const mapUtil = {
 			.attr('d', (d) => {
 				return path(d);
 			})
-			.attr('stroke', () => {
-				let color = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)].join();
-				console.log('rgb(' + color + ')');
-				return 'rgb(' + 
-					[Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)].join()
-					+ ')';
+			.attr('stroke', (d) => {
+				if (d.properties.name === NANHAI) {
+					return 'rgba(0, 0, 0, 0.3)';	
+				}
+				return this.color();
 			})
-			.attr('fill', () => {
-				return 'rgba(' + 
-					[Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), 
-						Math.random()].join() + ')';
+			.attr('fill', (d) => {
+				if (d.properties.name === NANHAI) {
+					return 'rgba(0, 0, 0, 0)';	
+				}
+				return this.color();	
 			});
+	},
+	color: function () {
+		return 'rgba(' + 
+			[Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), 
+				Math.random()].join() + ')';
 	}
 };
 export default mapUtil;
